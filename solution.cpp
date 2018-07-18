ee5c0b7fedd1a57ffb67047017746f1fee624b23
NelsonGomesNeto <ngn@ic.ufal.br>
2018-07-17 21:04:34 -0300

    Fixing some names

--- solution.cpp
+++ solution.cpp
@@ -9,11 +9,11 @@ class State
   public:
   vector<vector<int> > table;
   int step, blackI, blackJ;
-  double heuristic;
+  double distance;
 
-  State(vector<vector<int> > t, int bi, int bj, int s, double h)
+  State(vector<vector<int> > t, int bi, int bj, int s, double d)
   {
-    table = t, blackI = bi, blackJ = bj, step = s, heuristic = h;
+    table = t, blackI = bi, blackJ = bj, step = s, distance = d;
   }
 
   State(vector<vector<int> > t, int bi, int bj, int s)
@@ -23,12 +23,12 @@ class State
 
   bool operator()(const State& a, const State& b)
   {
-    return(a.heuristic < b.heuristic);
+    return(a.distance < b.distance);
   }
 
   bool operator<(const State& a) const
   {
-    return(heuristic > a.heuristic);
+    return(distance > a.distance);
   }
 };
 
